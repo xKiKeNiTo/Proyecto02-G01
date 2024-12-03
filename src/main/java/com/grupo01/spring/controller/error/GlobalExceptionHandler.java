13db35ed7d1e41faab7c95065f31159dfcc523f8
RaulMagre <raulgarciapaton@gmail.com>
2024-12-03 09:44:25 +0100

    M

--- src/main/java/com/grupo01/spring/controller/error/GlobalExceptionHandler.java
+++ src/main/java/com/grupo01/spring/controller/error/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.validation.FieldError;
 
@@ -24,5 +25,16 @@ public class GlobalExceptionHandler {
             .collect(Collectors.toList());
 
         return new ResponseEntity<>(Map.of("errors", errors), HttpStatus.BAD_REQUEST); // Devuelve los errores con el estado 400
+    }
+    
+    // Manejador de excepciones para métodos HTTP no soportados
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Object> handleMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+    	String metodoNoPermitido = ex.getMethod();
+    	String mensaje = String.format(
+    		"El método %s no está permitido en este endpoint. Métodos soportados: %s",
+    		metodoNoPermitido, ex.getSupportedHttpMethods());
+    	
+    	return new ResponseEntity<>(Map.of("errors", mensaje), HttpStatus.METHOD_NOT_ALLOWED);   	
     }
 }
